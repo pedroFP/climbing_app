f1f9adaf97fd22effdf060f4c4eee2efeec73622
Pedro <pedro@vinkodigital.com>
2023-11-09 22:49:51 -0300

    feat: :sparkles: generate devise user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_10_014532) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_014630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,13 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_10_014532) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
